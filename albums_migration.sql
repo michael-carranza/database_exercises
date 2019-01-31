f3933560ff8c67d3028100d01acc1c82a0f52429
Michael Carranza <michael.j.carranza@gmail.com>
2019-01-31 11:21:14 -0600

    index exercise completed

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
+    name unique VARCHAR(50) NOT NULL,
     release_date int,
     genre VARCHAR(50),
     sales float,
